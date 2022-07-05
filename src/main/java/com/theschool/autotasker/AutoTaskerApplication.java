52c2e971ceac407ba4874d9a574719d6c3909736
Thomas Munguya <thomas@thezig.io>
2022-07-05 21:21:34 +0200

    Initial commit.

---
+++ src/main/java/com/theschool/autotasker/AutoTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.theschool.autotasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoTaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theschool/autotasker/AutoTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theschool.autotasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
